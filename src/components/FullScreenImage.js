21398b417af8b7db6ebab39c0ff11d537d5e0dde
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-03-30 00:22:40 -0700

    updated full screen image zoom transition

--- src/components/FullScreenImage.js
+++ src/components/FullScreenImage.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+
 export default function FullScreenImage( {imgUrl, setActiveFullScreenImg, lightBackground} ) {
+
+    useEffect(() => {
+        const imageContainerEl = document.querySelector('.full_screen_img')
+        setTimeout(() => {
+            imageContainerEl.classList.add('active')
+        }, 0)
+    }, [])
 
     return(
         <div className={`full_screen_img ${lightBackground ? 'full_screen_img_light' : ''}`} onClick={() => setActiveFullScreenImg(false)}>
